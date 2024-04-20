15de73351df69924dc96784291c6ff32c931ed7c
davidSimek <david.simek@infis.cz>
2024-04-20 16:22:42 +0200

    change nasm linux

--- asm/Nasm_x86_64_Linux/01/main.asm
+++ asm/Nasm_x86_64_Linux/01/main.asm
@@ -1,1 +1,37 @@
+section .data
+    msg db "Hello from x86_64 Nasm for Linux.\n", 0
+    msg_len equ $ - msg
+
+section .text
+    global _start
+
+_start:
+    mov rdi, msg
+    mov rsi, msg_len
+    call print
+
+    mov rdi, 0
+    call exit
+    
+
+print:
+    ; rdi(string pointer)
+    ; rsi(string length)
+
+    mov rdx, rsi ;arg1
+    mov rsi, rdi ;arg0
+
+    mov rax, 1  ; write
+    mov rdi, 1  ; STDOUT
+    syscall
+
+    ret
+
+exit:
+    ; rdi = exit code
+
+    mov eax, 60 ; exit
+    syscall
+
+    ret
 
